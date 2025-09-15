2a582352322c3e5dd978c31a2a69fb4b6f4ea937
Alexander Hadion <gadion_alex@ukr.net>
2025-09-15 19:32:51 +0300

    fix link image

--- src/components/DefaultImage/DefaultImage.tsx
+++ src/components/DefaultImage/DefaultImage.tsx
@@ -3,11 +3,7 @@ import css from './DefaultImage.module.css';
 function DefaultImage() {
   return (
     <div className={css.default_image}>
-      <img
-        className={css.default_image_icon}
-        src="/public/film.svg"
-        alt="No poster"
-      />
+      <img className={css.default_image_icon} src="/film.svg" alt="No poster" />
       <h2 className={css.default_image_title}>No poster</h2>
     </div>
   );
